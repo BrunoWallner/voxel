a616093c773ddc2c83cdbe64ede61a2df8bf1056
luca <b.lucab1211@gmail.com>
2021-03-12 20:12:34 +0100

    added support for random chunk generation with OpenSimplex noise

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
 use bevy::prelude::*;
-use bevy::input::{keyboard::KeyCode, Input};
+
+use bevy_flycam::PlayerPlugin;
+use bevy_flycam::MovementSettings;
+
+use noise::NoiseFn;
+use noise::OpenSimplex;
 
 use rand::Rng;
+use rand::thread_rng;
+
 
 struct Cube;
-struct Camera;
+struct Chunk;
 
-struct Rotation { 
-    x: f32,
-    y: f32,
-    z: f32,
-}
+struct Seed {value: f64}
 
 fn main() {
     App::build()
         // Set WindowDescriptor Resource to change title and size
         .add_resource(WindowDescriptor {title: "Voxel!".to_string(), width: 1200.0, height: 800.0, ..Default::default()})
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup.system())
-        .add_startup_system(spawn_cubes.system())
+        .add_plugin(PlayerPlugin)
+        .add_resource(MovementSettings {
+            sensitivity: 0.00005, // default: 0.00012
+            speed: 15.0, // default: 12.0
+        })
 
-        .add_system(rotate_cube.system())
-        .add_system(camera_controll.system())
+        .add_resource(Seed { value: thread_rng().gen() })
+        .add_startup_system(setup.system())
+        .add_startup_system(spawn_chunk.system())
+
+        .add_system(rotate_chunk.system())
+
         .run();
 }
 
@@ -30,66 +40,58 @@ fn setup(
     commands: &mut Commands,
 ) {
     commands
-        // Camera
-        .spawn(Camera3dBundle {
-            transform: Transform::from_translation(Vec3::new(4., 0., 4.)),
-            ..Default::default()
-        })
-        .with(Camera)
-        .with(Rotation { x: 0.0, y: 20.0, z: 0.0})
-
-        // Light
         .spawn(LightBundle {
-            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+            transform: Transform::from_translation(Vec3::new(0.0, 100.0, 0.0)),
             ..Default::default()
         });
 }
 
-fn spawn_cubes(
+fn spawn_chunk(
     commands: &mut Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    seed: Res<Seed>,
 ) {
-    for _ in 0 .. 1000 {
-        let x = rand::thread_rng().gen_range(-100. .. 100.);
-        let y = rand::thread_rng().gen_range(-100. .. 100.);
-        let z = rand::thread_rng().gen_range(-100. .. 100.);
+    let noise = OpenSimplex::new();
 
-        commands
-            .spawn(PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Cube { size: 3.0 })),
-                material: materials.add(Color::rgb(1., 0.5, 0.5).into()),
-                transform: Transform::from_translation(Vec3::new(x, y, z)),
-                ..Default::default()
-            })
-            .with(Cube)
-            .with(Rotation { x: 0.0, y: 0.0, z: 0.0 });
-    }
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane{ size: 1.0 })),
+            material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            ..Default::default()
+        })
+        .with(Chunk)
+        .with_children(|parent| {
+    
+        for x in -32 .. 32 {
+            for z in -32 .. 32 {
+                let mut y = (noise.get([
+                    ( x as f32 / 20. ) as f64, 
+                    ( z as f32 / 20. ) as f64,
+                    seed.value,
+                ]) * 20.) as u32 + 16;
+
+
+                parent
+                    .spawn(PbrBundle {
+                        mesh: meshes.add(Mesh::from(shape::Cube{ size: 1.0 })),
+                        material: materials.add(Color::rgb(1., 0.5, 0.5).into()),
+                        transform: Transform::from_translation(Vec3::new(x as f32, y as f32, z as f32)),
+                        ..Default::default()
+                    })
+                .with(Cube);
+            }
+        }
+    });
 }
 
-fn rotate_cube(
-    mut cube: Query<(&mut Transform, &mut Rotation), With<Cube>>,
+fn rotate_chunk(
+    mut chunks: Query<&mut Transform, With<Chunk>>,
     time: Res<Time>,
 ) {
-    for (mut cube, mut rotation) in cube.iter_mut() {
-        rotation.y += 1.0 * time.delta_seconds();
-        cube.rotation = Quat::from_rotation_y(rotation.y);
+    for mut chunk in chunks.iter_mut() {
+        chunk.rotation *= Quat::from_rotation_y(0.25 * time.delta_seconds());
     }
-}
 
-fn camera_controll(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut camera: Query<(&mut Transform, &mut Rotation), With<Camera>>,
-    time: Res<Time>,
-) {
-    for (mut camera, mut rotation) in camera.iter_mut() {
-        if keyboard_input.pressed(KeyCode::A) {
-            rotation.y += 1.0 * time.delta_seconds();
-            camera.rotation = Quat::from_rotation_y(rotation.y);
-        }
-        if keyboard_input.pressed(KeyCode::D) {
-            rotation.y -= 1.0 * time.delta_seconds();
-            camera.rotation = Quat::from_rotation_y(rotation.y);
-        }
-    }
 }
