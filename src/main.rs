f8631afbf56042a0f8a1d12885b1cc0acf6b6bb9
luca <b.lucab1211@gmail.com>
2021-03-14 18:13:19 +0100

    finally added index based chunk spawning

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,12 @@ struct Cube;
 struct Chunk {
     pub x: i32,
     pub z: i32,
-    pub index: [[[u8; 16]; 16]; 256]
+    pub index: [[[u8; 16]; 256]; 16]
 }
 impl Chunk {
     //creates a new empty chunk filld with air
     pub fn new(x: i32, z: i32) -> Self {
-        Chunk {x: x, z: z, index: [[[0u8; 16]; 16]; 256]}
+        Chunk {x: x, z: z, index: [[[0u8; 16]; 256]; 16]}
     }
 }
 
@@ -35,11 +35,15 @@ fn main() {
 
         .add_startup_system(setup.system())
 
-        .add_startup_stage("ChunkSpawn", SystemStage::serial())
-        .add_startup_system_to_stage("ChunkSpawn",spawn_chunk.system())
+        .add_startup_stage("ChunkCreation", SystemStage::serial())
+        .add_startup_system_to_stage("ChunkCreation",create_chunk.system())
 
-        .add_startup_stage_after("ChunkSpawn", "ChunkGeneration", SystemStage::serial())
+        .add_startup_stage_after("ChunkCreation", "ChunkGeneration", SystemStage::serial())
         .add_startup_system_to_stage("ChunkGeneration",generate_chunk.system())
+
+        .add_startup_stage_after("ChunkGeneration", "ChunkSpawning", SystemStage::serial())
+        .add_startup_system_to_stage("ChunkSpawning",spawn_chunk.system())
+
 
         .run();
 }
@@ -59,7 +63,7 @@ fn setup(
         .spawn(Camera3dBundle {
             transform: Transform::from_matrix(Mat4::from_rotation_translation(
                 Quat::from_xyzw(-0.15, -0.5, -0.15, 0.5).normalize(),
-                Vec3::new(-125.0, 75.0, -12.5),
+                Vec3::new(-125.0, 75.0, -8.0),
             )),
             ..Default::default()
         });
@@ -74,7 +78,7 @@ fn setup(
 
 }
 
-fn spawn_chunk(
+fn create_chunk(
     commands: &mut Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     materials: Res<Materials>,
@@ -97,50 +101,56 @@ fn spawn_chunk(
 }
 
 fn generate_chunk(
+    mut chunks: Query<&mut Chunk, With<Chunk>>,
+    seed: Res<Seed>,
+) {
+    let chunk_size: usize = 16;
+    let noise = OpenSimplex::new();
+
+    for mut chunk in chunks.iter_mut() {
+        for x in 0 .. chunk_size {
+            for z in 0 .. chunk_size {
+                
+                let rng = thread_rng().gen_range(1 .. 10000);
+
+                let y = (noise.get([
+                    ( (x as i32 + chunk.x * chunk_size as i32) as f32 / 20. ) as f64, 
+                    ( (z as i32 + chunk.z * chunk_size as i32) as f32 / 20. ) as f64,
+                    seed.value * 10000.0,
+                ]) * 20. + 5.0) as usize;
+
+                chunk.index[x][y][z] = 1;
+
+            }
+        }
+        
+    }
+}
+
+fn spawn_chunk(
     commands: &mut Commands,
     chunks: Query<&Chunk, With<Chunk>>,
     materials: Res<Materials>,
     mut meshes: ResMut<Assets<Mesh>>,
-    seed: Res<Seed>,
 ) {
-    let chunk_size = 16;
-    let noise = OpenSimplex::new();
-
-
     for chunk in chunks.iter() {
 
-        for x in -chunk_size .. chunk_size {
-            for z in -chunk_size .. chunk_size {
+        for x in 0 .. 16 {
+            for y in 0 .. 255 {
+                for z in 0 .. 16 {
 
-                let rng = thread_rng().gen_range(1 .. 10000);
-
-                let y = (noise.get([
-                    ( (x + chunk.x * chunk_size) as f32 / 20. ) as f64, 
-                    ( (z + chunk.z * chunk_size) as f32 / 20. ) as f64,
-                    seed.value * 10000.0,
-                ]) * 15. + 16.0) as u32;
-
-
-                commands
-                    .spawn(PbrBundle {
-                       mesh: meshes.add(Mesh::from(shape::Cube{ size: 1.0 })),
-                        material: materials.grass.clone(),
-                        transform: Transform::from_translation(Vec3::new((x + chunk.x * chunk_size) as f32, y as f32, (z + chunk.z * chunk_size) as f32)),
-                        ..Default::default()
-                    })
-                    .with(Cube);
-
-                if rng % 2000 == 0 {
-                    commands
-                        .spawn(PbrBundle {
-                            mesh: meshes.add(Mesh::from(shape::Cube{ size: 1.0 })),
-                            material: materials.redstone.clone(),
-                            transform: Transform::from_translation(Vec3::new((x + chunk.x * chunk_size) as f32, y as f32 + 1.0, (z + chunk.z * chunk_size) as f32)),
-                            ..Default::default()
-                        })
-                        .with(Cube);
-                }
+                    if chunk.index[x][y][z] == 1 {
+                        commands
+                            .spawn(PbrBundle {
+                                mesh: meshes.add(Mesh::from(shape::Cube{ size: 1.0 })),
+                                material: materials.grass.clone(),
+                                transform: Transform::from_translation(Vec3::new((x as i32 + chunk.x * 16) as f32, y as f32, (z as i32 + chunk.z * 16) as f32)),
+                                ..Default::default()
+                            })
+                            .with(Cube);
+                    }
+                }    
             }
         }
-    }
+    }  
 }
